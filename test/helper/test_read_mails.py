8435e88cc1b187515ff0fd164f1452cb52f2761f
Franz-Hermann Huerkamp <fhh@huerkamp.de>
2024-05-23 21:35:59 +0200

    Test: Read mails from File

--- test/helper/test_read_mails.py
+++ test/helper/test_read_mails.py
@@ -1,7 +1,7 @@
-from imap_reading.helper.read_mails import read_mails
+from imap_reading.helper.read_mails import read_mails_from_mailserver
 import os
 from dotenv import load_dotenv
-from imap_reading.helper.read_mails import read_mails
+from imap_reading.helper.read_mails import read_mails_from_mailserver, read_mails_from_file
 from datetime import date,timedelta
 
 
@@ -11,7 +11,7 @@ def test_read_mails():
     user=os.environ.get("TEST_IMAPUSER")
     password=os.environ.get("TEST_IMAPPASSWORD")
     sender=os.environ.get("TEST_SENDER")
-    mails = read_mails(host, user, password, sender)
+    mails = read_mails_from_mailserver(host, user, password, sender)
     assert len(mails) > 0
     for mail in mails:
         assert sender in mail.m_from
@@ -23,7 +23,7 @@ def test_read_mails_with_daybacks():
     password=os.environ.get("TEST_IMAPPASSWORD")
     sender=os.environ.get("TEST_SENDER")
     daysback = 2
-    mails = read_mails(host, user, password, sender, daysback=daysback)
+    mails = read_mails_from_mailserver(host, user, password, sender, daysback=daysback)
     date_check = date.today() - timedelta(days=daysback)
 
     assert len(mails) > 0
@@ -31,3 +31,18 @@ def test_read_mails_with_daybacks():
         # import ipdb; ipdb.set_trace()
         assert sender in mail.m_from
         assert date_check <= mail.m_date.date()
+
+
+def test_read_mails_from_file():
+    load_dotenv(override=False)
+    mail_in_file=os.environ.get("TEST_RAW_MAIL_IN_FILE")
+    sender=os.environ.get("TEST_SENDER")
+
+    mailcontent = read_mails_from_file(mail_file=mail_in_file)
+    # import ipdb; ipdb.set_trace()
+    assert sender in mailcontent.m_from
+
+
+
+# if __name__ == "__main__":
+#     test_read_mails_from_file()
