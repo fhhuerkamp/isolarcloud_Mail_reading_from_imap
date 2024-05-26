5f10f2c8560a8be19dab47a3a18d4a2d710408ce
Franz-Hermann Huerkamp <fhh@huerkamp.de>
2024-05-26 07:29:05 +0200

    add selenium reading

---
+++ imap_reading/helper/read_url_with_selenium.py
@@ -0,0 +1,52 @@
+# pip install selenium
+# pip install chromedriver-autoinstaller
+
+import chromedriver_autoinstaller
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+def read_url_with_selenium(url, list_css_elements,file_output=None):
+    """
+
+    reads an url with selenium and returns the generated html
+    waits until some css classes are in the code
+
+    when a name of an output file is given, then stores the html in that file
+
+
+    Example Call: read_html_with_selenium(
+        'https://example.com/html_with_javascript.html',
+        ['el-table_1_column_9','report-table'],
+        file_output='Output.html')
+
+    """
+
+    WAIT_TIME = 100     # max wait time for getting the CSS classes in the html-code
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless=new')
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    html = None
+    try:
+        wait = WebDriverWait(driver, WAIT_TIME)
+        for css_element in list_css_elements:
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, css_element)))
+        time.sleep(1)
+        html = driver.page_source
+        if file_output:
+            if len(html) > 100:
+                text_file = open(file_output, "w")
+                text_file.write(html)
+                text_file.close()
+    except Exception as ex:
+        print (f"######\n{ex.with_traceback}")
+    finally:
+        driver.quit()
+    return html
+
+if __name__ == "__main__":
+    pass
---
+++ test/helper/test_read_url_with_selenium.py
@@ -0,0 +1,17 @@
+from imap_reading.helper.read_url_with_selenium import read_url_with_selenium
+import os
+from dotenv import load_dotenv
+
+
+def test_read_url_with_selenium():
+    load_dotenv(override=False)
+    url=os.environ.get("EXAMPLE_REPORT_URL")
+    html = read_url_with_selenium(url,['report-table','el-table_1_column_9'],"test_output.html")
+    assert len(html) > 0
+    matches = ['23:00','report-table', 'el-table_1_column_9']
+    for teststring in matches:
+        assert teststring in html
+
+
+if __name__ == "__main__":
+    test_read_url_with_selenium()
